f297a579fb0bc3920951073c92204ca2c3658d79
C5P3 <elia04@gmx.ch>
2025-04-06 23:47:12 +0200

    Create guest.py
    initial class definition

---
+++ src/models/guest.py
@@ -0,0 +1,9 @@
+class Guest:
+  def _init_ (self, guest_id: int, name: str, surname: str, phone: int, email: str, zip: int, address: str)
+    self._id = guest_id
+    self._name = name
+    self._surname = surname
+    self._phone = phone
+    self._email = email
+    self._zip = zip
+    self._address = address
