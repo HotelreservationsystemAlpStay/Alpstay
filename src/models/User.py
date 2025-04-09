d76b86241dc65f76e09ad84312dfd69e04d8eeab
ZenoRauch <zeno@rauch-nauer.ch>
2025-04-09 15:59:39 +0200

    Changes attributes to public, changed language from german to english, corrected user.py

--- src/models/User.py
+++ src/models/User.py
@@ -1,68 +1,70 @@
 class User:
-    def __init__(self, id:int, username: str, password: str, role: str)-> None: 
+    def __init__(self, id: int, username: str, password: str, role: str) -> None:
         self._validateId(id)
         self._validateUsername(username)
         self._validatePassword(password)
         self._validateRole(role)
-        
-        self._id = id
-        self._username = username
-        self._password = password
-        self._role = role
-    
+
+        self.id = id
+        self.username = username
+        self.password = password
+        self.role = role
+
     def _validateId(self, id: int) -> None:
         if not isinstance(id, int):
-            raise TypeError("ID muss eine Ganzzahl sein")
+            raise TypeError("ID must be an integer")
         if id <= 0:
-            raise ValueError("ID muss positiv sein")
+            raise ValueError("ID must be positive")
 
     def _validateUsername(self, username: str) -> None:
         if not isinstance(username, str):
-            raise TypeError("Benutzername muss ein String sein")
+            raise TypeError("Username must be a String")
         if not username.strip():
-            raise ValueError("Benutzername darf nicht leer sein")
-    
+            raise ValueError("Username must not be empty")
+
     def _validatePassword(self, password: str) -> None:
         if not isinstance(password, str):
-            raise TypeError("Password muss ein String sein")
+            raise TypeError("Password must be a String")
         if not password.strip():
-            raise ValueError("Password darf nicht leer sein")
-        
+            raise ValueError("Password must not be empty")
+
     def _validateRole(self, role: str) -> None:
         if not isinstance(role, str):
-            raise TypeError("Rolle muss ein String sein")
+            raise TypeError("Role must be a String")
         if not role.strip():
-            raise ValueError("Rolle darf nicht leer sein")
-        
+            raise ValueError("Role must not be empty")
+
+
+""" 
     @property
     def getId(self) -> int:
-        return self._id
+        return self.id
 
     @getId.setter
     def setId(self, id: int) -> None:
-        self._id = id
+        self.id = id
 
     @property
     def getUsername(self) -> str:
-        return self._username
+        return self.username
 
     @getUsername.setter
     def setDescription(self, username: str) -> None:
-        self._username = username
+        self.username = username
     
     @property
     def getPassword(self) -> str:
-        return self._password
+        return self.password
 
     @getPassword.setter
     def setDescription(self, password: str) -> None:
-        self._password = password
+        self.password = password
     
     @property
     def getRole(self) -> str:
-        return self._role
+        return self.role
 
     @getRole.setter
     def setDescription(self, role: str) -> None:
-        self._role = role
-        
+        self.role = role
+        """
--- src/models/guest.py
+++ src/models/guest.py
@@ -1,28 +1,29 @@
-#definition der Klasse Guest und allfällige Fehlermeldungen bei nicht korrektem gebrauch des Datentyp 
+# definition der Klasse Guest und allfällige Fehlermeldungen bei nicht korrektem gebrauch des Datentyp
 class Guest:
-  def _init_ (self, guest_id: int, name: str, surname: str, phone_nr: int, email: str, zip: int, address: str)
-      if not isinstance(guest_id, int):
-        raise ValueError("Die Guest-ID muss eine Integer sein")
-      if not isinstance(name, str):
-        raise ValueError("Der Name muss eine String sein")
-      if not isinstance(surname, str):
-        raise ValueError("Der Surname muss eine String sein")
-      if not isinstance(phone_nr, int):
-        raise ValueError("Die Phone_nr muss eine Integer sein")
-      if not isinstance(email, str):
-        raise ValueError("Die Email muss eine String sein")
-      if not isinstance(zip, int):
-        raise ValueError("Die ZIP muss eine Integer sein")
-      if not isinstance(address, str):
-        raise ValueError("Die Address muss eine String sein")
-      self._id = guest_id
-      self._name = name
-      self._surname = surname
-      self._phone_nr = phone_nr
-      self._email = email
-      self._zip = zip
-      self._address = address
-#Angaben die Privat für den Admin sind
-  @property
-  def guest_id(self):
-    return self._guest_id
+    def _init_(self, guest_id: int, name: str, surname: str, phone_nr: int, email: str, zip: int, address: str):
+        if not isinstance(guest_id, int):
+            raise ValueError("Die Guest-ID muss eine Integer sein")
+        if not isinstance(name, str):
+            raise ValueError("Der Name muss eine String sein")
+        if not isinstance(surname, str):
+            raise ValueError("Der Surname muss eine String sein")
+        if not isinstance(phone_nr, int):
+            raise ValueError("Die Phone_nr muss eine Integer sein")
+        if not isinstance(email, str):
+            raise ValueError("Die Email muss eine String sein")
+        if not isinstance(zip, int):
+            raise ValueError("Die ZIP muss eine Integer sein")
+        if not isinstance(address, str):
+            raise ValueError("Die Address muss eine String sein")
+        self._id = guest_id
+        self._name = name
+        self._surname = surname
+        self._phone_nr = phone_nr
+        self._email = email
+        self._zip = zip
+        self._address = address
+# Angaben die Privat für den Admin sind
+
+    @property
+    def guest_id(self):
+        return self._guest_id
