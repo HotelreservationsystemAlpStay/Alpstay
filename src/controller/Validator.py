a5b3a3925c434aa8fb6c97c3d3a5ed8dad4225e4
ZenoRauch <zeno@rauch-nauer.ch>
2025-04-30 08:33:20 +0200

    altered checkID-function to take optional name parameter

--- src/controller/Validator.py
+++ src/controller/Validator.py
@@ -1,25 +1,26 @@
-#@staticmethod ermöglicht es die Methoden ohne das Objekt aufzurufen, also wenn ich z. B. bei Hotel sternen prüfen möchte
-#kann ich einfach sagen Validator.checkInteger(min_stars)
+# @staticmethod ermöglicht es die Methoden ohne das Objekt aufzurufen, also wenn ich z. B. bei Hotel sternen prüfen möchte
+# kann ich einfach sagen Validator.checkInteger(min_stars)
 from datetime import date
+
 
 class Validator:
 
     @staticmethod
     def checkInteger(value: int, name: str):
         if not isinstance(value, int):
-                raise ValueError(f"{name} must be a whole number")
-    
+            raise ValueError(f"{name} must be a whole number")
+
     @staticmethod
     def checkPositiveInteger(value: int, name: str):
         Validator.checkInteger(value, name)
         if value < 0:
-             raise ValueError(f"{name} must be positive")
+            raise ValueError(f"{name} must be positive")
 
     @staticmethod
     def checkFloat(value: float, name: str):
         if not isinstance(value, float):
             raise ValueError(f"{name} must be a float")
-    
+
     @staticmethod
     def checkPositiveFloat(value: float, name: str):
         Validator.checkFloat(value, name)
@@ -27,38 +28,38 @@ class Validator:
             raise ValueError(f"{name} must be positive")
 
     @staticmethod
-    def checkStars(value:int):
-         Validator.checkInteger(value, "Stars")
-         if value < 1 or value > 5:
-              raise ValueError("Stars must be between 1 and 5")
-        
-    @staticmethod    
-    def checkID(value: int):
-        Validator.checkInteger(value, "ID")
-        if value <= 0:
-            raise ValueError("ID must be a positive number")
-        
+    def checkStars(value: int):
+        Validator.checkInteger(value, "Stars")
+        if value < 1 or value > 5:
+            raise ValueError("Stars must be between 1 and 5")
+
     @staticmethod
-    def checkStr(value: str, name:str):
+    def checkID(value: int, name="ID"):
+        Validator.checkInteger(value, name)
+        if value <= 0:
+            raise ValueError(f"{name} must be a positive number")
+
+    @staticmethod
+    def checkStr(value: str, name: str):
         if not isinstance(value, str):
             raise ValueError(f"{name} has to be of type string")
         if not value.strip():
             raise ValueError(f"{name} must not be empty")
-        
+
     @staticmethod
     def checkDate(value: date, name: str):
         if not isinstance(value, date):
             raise ValueError(f"{name} has to be of type datetime.date")
-    
+
     @staticmethod
-    def checkDateDifference(valueFirstDate: date, valueSecondDate:date):
+    def checkDateDifference(valueFirstDate: date, valueSecondDate: date):
         Validator.checkDate(valueFirstDate)
         Validator.checkDate(valueSecondDate)
         if valueSecondDate > valueFirstDate:
             raise ValueError("Last Date is before first Date")
-    
+
     @staticmethod
-    def checkDateFormat(value: date, name:str):
+    def checkDateFormat(value: date, name: str):
         if not isinstance(value, date):
             raise ValueError(f"{name} has to be of type date")
         if not value.strip():
@@ -67,14 +68,14 @@ class Validator:
             date.fromisoformat(value)
         except ValueError:
             raise ValueError(f"{name} is not a valid date format (YYYY-MM-DD)")
-        
+
     @staticmethod
     def checkDates(check_in_date: date, check_out_date: date):
         if check_in_date >= check_out_date:
             raise ValueError("Check-in date must be before check-out date")
-        
+
     @staticmethod
-    def checkBoolean(value: bool, name:str):
+    def checkBoolean(value: bool, name: str):
         if not isinstance(value, bool):
             raise ValueError(f"{name} has to be of type boolean")
         if not value.strip():
