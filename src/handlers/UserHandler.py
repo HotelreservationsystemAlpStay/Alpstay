e57699f398edc29ced36041ac7c666a8e18735a4
Yves Noé Bornhauser <Yves.bornhauser@students.fhnw.ch>
2025-04-30 22:53:23 +0200

    Created Userhandler and implemented logic that checks if youre an admin

---
+++ src/handlers/UserHandler.py
@@ -0,0 +1,41 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from controller.DataBaseController import DataBaseController
+from models.User import User
+import hashlib
+
+class Userhandler:
+    def __init__(self):
+        self.db = DataBaseController()
+
+    def check_admin(self, user_id, password):
+        query = """
+        SELECT *
+        FROM User
+        WHERE user_id = ?
+        """
+        result = self.db.fetchone(query,(user_id,))
+        if result is None:
+            raise ValueError("We couldnt find your user_id, please try again")
+        data = dict(result)
+        user = User(id = data["user_id"], guest_id = data["guest_id"], password = data["user_password"], role = data["user_role"])
+        password_hash = hashlib.sha256(password.encode()).hexdigest()
+        if user.password == password_hash: 
+            if user.role == "Admin":
+                print("Admin Access granted")
+                return True
+            elif user.role == "Guest":
+                print("Guest Access granted")
+                return False
+            else:
+                raise ValueError("User Role not defined, please contact the admin and report this issue")
+        else: 
+            raise ValueError("The password you entered is wrong, please try again")
+
+#Test
+test1 = Userhandler()
+test1.check_admin(6, "admin")
+
+
+
