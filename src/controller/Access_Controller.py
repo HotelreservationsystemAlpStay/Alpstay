0d90ff310ee1f18a66643f263c4f3f2f0ce578ca
BastiBuilds <bastian.bueeler@gmail.com>
2025-05-07 16:27:23 +0200

    Refactor Access_Controller and ConsoleView to replace service imports with data access imports

--- src/controller/Access_Controller.py
+++ src/controller/Access_Controller.py
@@ -1,18 +1,18 @@
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from services.BookingService import BookingService
-from services.UserService import UserService
-from services.RoomTypeService import RoomTypeServie
-from services.RoomService import RoomService
-from services.InvoiceService import Invoiceservice
-from services.HotelService import Hotelservice
+from data_Access.Booking_Access import Booking_Access
+from data_Access.User_Access import User_Access
+from data_Access.RoomType_Access import RoomTypeServie
+from data_Access.Room_Access import Room_Access
+from data_Access.Invoice_Access import Invoice_Access
+from data_Access.Hotel_Access import Hotel_Access
 
-class ServiceHandler:
+class Access_Controller:
     def __init__(self):
-        self.booking_service = BookingService()
-        self.user_service = UserService()
-        self.room_type_service = RoomTypeServie()
-        self.room_service = RoomService()
-        self.invoice_service = Invoiceservice()
-        self.hotel_service = Hotelservice()
+        self.booking_Access = Booking_Access()
+        self.user_Access = User_Access()
+        self.room_type_Access = RoomTypeServie()
+        self.room_Access = Room_Access()
+        self.invoice_Access = Invoice_Access()
+        self.hotel_Access = Hotel_Access()
--- src/views/ConsoleView.py
+++ src/views/ConsoleView.py
@@ -1,12 +1,12 @@
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from handlers.ServiceHandler import ServiceHandler
+from controller.Access_Controller import Access_Controller
 from datetime import datetime, date
 
 class ConsoleView:
     def __init__(self):
-        self.service = ServiceHandler()
+        self.service = Access_Controller()
 
     def run(self):
         while True:
