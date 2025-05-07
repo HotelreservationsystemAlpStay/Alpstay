cf64dc5e34c6f9d99e754fb5e8f4068567748dda
BastiBuilds <bastian.bueeler@gmail.com>
2025-05-07 16:39:25 +0200

    added Manu options as an example to ConsoleView

--- src/views/ConsoleView.py
+++ src/views/ConsoleView.py
@@ -1,21 +1,31 @@
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from controller.Access_Controller import Access_Controller
+#from controller.Access_Controller import Access_Controller
 from datetime import datetime, date
 
 class ConsoleView:
     def __init__(self):
-        self.service = Access_Controller()
+        #self.service = Access_Controller()
+        print("ConsoleView initialized")
 
     def run(self):
         while True:
             print("\nAlpsty Hotelreservationsystem Console")
-            print("1.")
+            print("1. Hello World")
+            print("2. Show all guests")
+            print("3. Show all rooms")
+            print("4. Show all bookings")
             print("q. Quit")
             choice = input("Select an option: ")
             if choice == '1':
                 print("1.")
+            elif choice == '2':
+                print("Showing all guests...")
+            elif choice == '3':
+                print("Showing all rooms...")
+            elif choice == '4':
+                print("Showing all bookings...")
             elif choice.lower() == 'q':
                 print("Exiting.")
                 break
