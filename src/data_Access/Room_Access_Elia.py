35b4991d4d78c769da7e810cb0ec8e3abab1af01
ZenoRauch <zeno@rauch-nauer.ch>
2025-05-11 17:49:17 +0200

    userstory 7, added seasonal pricing on rooms

--- src/data_Access/Room_Access_Elia.py
+++ src/data_Access/Room_Access_Elia.py
@@ -1,5 +1,9 @@
-class RoomAccess():
+from datetime import date
 
+class RoomAccess():
+    def __init__(self, summer:float, winter:float):
+        self._summer = summer
+        self._winter = winter
 
     @staticmethod
     def _get_seasonal_multiplier(season:str):
@@ -9,29 +13,33 @@ class RoomAccess():
         elif season == "Winter":
             return 1.0 
 
-    #Idee:
     @staticmethod
     def _get_season(date:date):
-    
+        """_summary_
+
+        Args:
+            date (date): _description_
+
+        Returns:
+            _type_: _description_
+        """
         date_now = date.strftime("%m-%d")
         summer_start = "04-01"
         summer_end = "09-30"
         winter_start = "10-01"
         winter_end = "03-31"
 
-        if summer_start <= date_now <= summer_end:
+        if summer_start <= date.strftime("%m-%d") <= summer_end:
             return "Summer"
         elif winter_start <= date_now or date_now <= winter_end:
             return "Winter"
+        
+    
 
     @staticmethod
     def _calculate_days(start_date, end_date):
         days = (end_date - start_date).days
         return days
-        
-    def __init__(self, summer:float, winter:float):
-        self._summer = summer
-        self._winter = winter
 
     def set_price(self,price:float, season:str):
         if season == "summer":
@@ -39,8 +47,9 @@ class RoomAccess():
         else: 
             return price*self._winter
     
-    def price_per_stay():
+    def price_per_stay() -> None:
+        pass
         # def set_price * days oder so
         
-roomAccess = RoomAccess(1.9, 1.0)
-print(roomAccess.set_price(150, "summer"))
+
+print(date(day=21, month=4, year=2025).strftime("%m"))
