a644a77d88bdd2c71e9957f017a5956d25d145e9
ZenoRauch <zeno@rauch-nauer.ch>
2025-05-16 06:49:16 +0200

    [Unstable] Refactoring RoomAccess

--- src/data_Access/Room_Access.py
+++ src/data_Access/Room_Access.py
@@ -49,7 +49,6 @@ class Room_Access:
         return "Winter"
 
     def _sqlite3row_to_room(self, row: sqlite3.Row, facilities=[], roomType:RoomType=None, season:str=None) -> Room:
-        print(str(row))
         return Room(
             room_id=row["room_id"],
             room_no=row["room_number"],
@@ -89,13 +88,32 @@ class Room_Access:
         """
         if not dateStart:
             return query, param
-        if "AND" not in query: query +=f" {self._AND}"
+        if "WHERE" in query: query +=f" {self._AND}"
+        if "AND" not in query: query +=f" {self._WHERE}"
         query += f" {self._WHERE_BOOKINGDATE}"
         param.extend([dateStart,dateEnd,dateStart,dateEnd,dateStart,dateEnd])
         return query,param
 
-    def add_hotel_ids(self, query:str, param: list, hotel_ids: list[int]):
-        pass
+    def _add_hotel_ids(self, query:str, param: list, hotel_ids: list[int]):
+        if not hotel_ids or len(hotel_ids) < 1:
+            return query,param
+        if "WHERE" in query: query +=f" {self._AND}"
+        if "AND" not in query: query +=f" {self._WHERE}"
+        if len(hotel_ids) == 1:
+            query += f" {self._WHERE} {self._WHERE_HOTELID} ({hotel_ids[0]})"
+        else:    
+            query += f" {self._WHERE} {self._WHERE_HOTELID} {tuple(hotel_ids)}"
+        return query, param
+
+    def _add_roomType(self, query:str, param:list, roomType:RoomType):
+        if not roomType or not isinstance(roomType, RoomType):
+            return query,param
+        if "WHERE" in query: query +=f" {self._AND}"
+        if "AND" not in query: query +=f" {self._WHERE}"
+        query += f" {self._WHERE_ROOMTYPE}"
+        param.append(roomType.id)
+        return query, param
+                    
 
     def get_rooms(
         self, dateStart: date = None, dateEnd: date = None, hotel_ids: list[int] = None, roomType:RoomType = None
@@ -110,29 +128,11 @@ class Room_Access:
         Returns:
             list[Room]: rooms filtered on provided args
         """
-        
-        inLoop = False
         providedList = []
         query = self._SELECT
         query, providedList = self._add_dates(query, providedList,dateStart,dateEnd)
-        if hotel_ids and len(hotel_ids) > 1:
-            if inLoop: 
-                query += f" AND {self._WHERE_HOTELID} {tuple(hotel_ids)}"
-            else:
-                inLoop = True
-                query += f" {self._WHERE} {self._WHERE_HOTELID} {tuple(hotel_ids)}"
-        elif hotel_ids and len(hotel_ids) == 1:
-            if inLoop: query += f" AND {self._WHERE_HOTELID} ({hotel_ids[0]})"
-            else:
-                inLoop = True 
-                query += f" {self._WHERE} {self._WHERE_HOTELID} ({hotel_ids[0]})"
-        if roomType:
-            if inLoop:
-                query +=f" {self._WHERE} {self._WHERE_ROOMTYPE}"
-            else: 
-                inLoop = True
-                query +=f" AND {self._WHERE} {self._WHERE_ROOMTYPE}"
-            providedList.append(roomType.id)
+        query, providedList = self._add_hotel_ids(query,providedList,hotel_ids)
+        query, providedList = self._add_roomType(query,providedList, roomType)
         print(f"query {query}")
         print(providedList)
         results = self.db.fetchall(
@@ -151,74 +151,34 @@ class Room_Access:
         return rooms
     
 
-"""
-    def calculate_room_price_per_stay(self, price_per_night, start_date, end_date):
-        query = "SELECT price_per_night FROM room WHERE room_id = ?"
-        result = self.db.fetchone(query, (room_id,))
-
-        price_per_night = result[0]
-
-        if isinstance(input_date, str):
-            input_date = datetime.strptime(input_date, "%Y-%m-%d").date()
-
-            start_date = (input_date.month, input_date.day)
-
-        spring_season_start = (3-1)
-        spring_season_end = (5-31)
-        summer_season_start = (6-1)
-        summer_season_end = (8-31)
-        fall_season_start = (9-1)
-        fall_season_end = (11-30)
-        winter_season_start = (12-1)
-        winter_season_end = (2-28)
-
-        if start_date >= spring_season_start and start_date <= spring_season_end:
-            season = "Spring Season"
-        elif start_date >= summer_season_start and start_date <= summer_season_end:
-            season = "Summer Season"
-        elif start_date >= fall_season_start and start_date <= fall_season_end:
-            season = "Fall Season"
-        elif start_date >= winter_season_start and start_date <= winter_season_end:
-            season = "Winter Season"
-        elif start_date >= "01-01" and start_date <= "02-28":
-            season = "Winter Season"
-       
-        if season == "Spring Season":
-            multiplier = 0.85
-        elif season == "Summer Season":
-            multiplier = 1.5
-        elif season == "Fall Season":
-            multiplier = 0.85
-        elif season == "Winter Season":
-            multiplier = 1.2
-        else: multiplier = 1.0
-
-        days = end_date - start_date
-
-        final_price_per_stay = price_per_night * multiplier * days 
-        # mit dieser Variante erhällt man für alle Tage die Preise der Saison, in der man begonnen hat mit dem Aufenthalt
-        # Es werden so Kunden belohnt, die in der Nebensaison buchen und evtl. auch länger bleiben und Kunden bestraft, die in der Hochsaison buchen
-        return final_price_per_stay
-    
-
-    # Task Elia: Anzahl Tage einfügen (evtl. Durchschnitt der Preise zusammenrechnen und durch Anzahl Tage teilen, sodass man auch in verscheidenen Saisons einen korrekten Preis erhällt)
-    
 
 
-print("### 2")
-rs.get_available_rooms(date(2025, 6, 1), date(2025, 6, 5), hotel_ids=[2])
-print("### 3")
-rs.get_available_rooms(date(2025, 6, 1), date(2025, 6, 5), hotel_ids=[2, 3, 5])
-
-
-"""
-"""
 rs = Room_Access()
 print("### 1")
-tempRooms = rs.get_available_rooms(date(2025, 6, 1), date(2025, 6, 5))
+tempRooms = rs.get_rooms(date(2025, 6, 1), date(2025, 6, 5))
 for room in tempRooms:
     print(room)
     for facility in room.facilities:
         print(facility)
     print(room.roomType)
-"""
+print("### 2")
+tempRooms = rs.get_rooms(date(2025, 6, 1), date(2025, 6, 5), hotel_ids=[2])
+for room in tempRooms:
+    print(room)
+    for facility in room.facilities:
+        print(facility)
+    print(room.roomType)
+print("### 3")
+tempRooms = rs.get_rooms(date(2025, 6, 1), date(2025, 6, 5), hotel_ids=[2, 3, 5])
+for room in tempRooms:
+    print(room)
+    for facility in room.facilities:
+        print(facility)
+    print(room.roomType)
+print("### 4")
+tempRooms = rs.get_rooms(date(2025, 6, 1), date(2025, 6, 5), hotel_ids=[2, 3, 5], roomType=RoomType(4,"stuff",2))
+for room in tempRooms:
+    print(room)
+    for facility in room.facilities:
+        print(facility)
+    print(room.roomType)
