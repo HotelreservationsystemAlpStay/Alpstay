6f7bdc25b7462cc04f295be4c7d0f628dbb067bf
C5P3 <elia04@gmx.ch>
2025-05-16 14:27:14 +0200

    Update RoomType_Controller.py
    Updated Syntax at RoomType Controller

--- src/controller/RoomType_Controller.py
+++ src/controller/RoomType_Controller.py
@@ -1,18 +1,27 @@
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from utils.Validator import Validator
+from data_Access.Base_Access_Controller import Base_Access_Controller
 from data_Access.RoomType_Access import RoomType_Access
 from controller.User_Controller import User_Controller
 
 class RoomType_Controller():
     def __init__(self):
-        pass
+        pass 
 
-    def add_roomType(self, user_id:int, password:str, description:str=None, max_guest:int=None):
+    def add_roomType(self, user_id:int, password:str, description:str, max_guests:int):
         user_Controller = User_Controller()
-        if not user_Controller(user_id, password):
-            return True
-        else:
+        if not user_Controller.check_admin(user_id, password):
             return False
+        roomType_Access = RoomType_Access()
+        return roomType_Access.add_roomtype()
+            
+        
 
-
+    def modify_roomType(self, user_id:int, password:str, description:str=None, max_guests:int=None, type_id):
+        user_Controller = User_Controller()
+        if not user_Controller.check_admin(user_id, password):
+            return False
+        roomType_Access = RoomType_Access()
+        return roomType_Access.modify_roomtype()
