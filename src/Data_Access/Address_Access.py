83290a8c5c220c3edbc8896ede3d5bf1bc80e548
Basti <149380126+BastiBuilds@users.noreply.github.com>
2025-05-21 10:13:29 +0200

    Delete src/Data_Access directory

--- src/Data_Access/Address_Access.py
+++
@@ -1,9 +0,0 @@
-from models.Address import Address
-from Data_Access.Base_Access_Controller import Base_Access_Controller
-
-class Address_Access:
-    def __init__(self):
-        self.base_access_controller = Base_Access_Controller()
-
-    def getAddress(id):
-        pass
