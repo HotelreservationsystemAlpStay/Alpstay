9f6fc46d3bbc442ca6ad75f57bc7c8156873cbee
BastiBuilds <bastian.bueeler@gmail.com>
2025-05-21 10:49:39 +0200

    Remove unused ConsoleView, Address_Access, and Main modules to streamline the codebase.

--- src/RunHotelReservationSystem.py
+++
@@ -1,6 +0,0 @@
-from views.Archive.ConsoleView import ConsoleView
-
-if __name__ == "__main__":
-    view = ConsoleView()
-    view.run()
-
--- src/views/Main.py
+++
@@ -1,7 +0,0 @@
-import sys
-import os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from views.StartMenu import StartMenu
-
-if __name__ == "__main__":
-    StartMenu().run()
