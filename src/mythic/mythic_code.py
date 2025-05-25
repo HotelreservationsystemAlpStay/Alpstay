f2278982e52b44fbeabf8e0fcca26e72f8cf6f19
Yves Noé Bornhauser <Yves.bornhauser@students.fhnw.ch>
2025-05-25 14:16:29 +0200

    I revolutionized our most important class

--- src/mythic/mythic_code.py
+++ src/mythic/mythic_code.py
@@ -1,6 +1,13 @@
 from PIL import Image
+import random
+
 class Mythic:
     @staticmethod
     def wtf():
-        image = Image.open("src/mythic/very_mythic.png")
+        number = random.randint(0, 1)
+        if number == 0:
+            path = "src/mythic/very_mythic.png"
+        elif number == 1:
+            path = "src/mythic/very_mythic_1.png"
+        image = Image.open(path)
         image.show()
